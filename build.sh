899892e46dd3de82bb75682648389d332c4613b9
Luke Dearden <lukejdearden@gmail.com>
2024-10-05 12:39:07 +0300

    more things

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -14,10 +13,64 @@ RELEASE="$(rpm -E %fedora)"
 
 # this installs a package from fedora repos
 rpm-ostree install screen
+# Install RPM packages
+rpm-ostree install \
+    adwaita-blue-gtk-theme \
+    adwaita-gtk2-theme \
+    adwaita-icon-theme \
+    adwaita-qt5 \
+    bash-completion \
+    btop \
+    blueman \
+    breeze-cursor-theme \
+    breeze-gtk \
+    breeze-icon-theme \
+    chrony \
+    firewall-config \
+    flatpak \
+    fontawesome-6-free-fonts \
+    fontawesome-6-brands-fonts \
+    fuzzel \
+    git \
+    gnome-keyring \
+    gnome-packagekit-installer \
+    gnome-software \
+    gvfs-smb \
+    gvfs-nfs \
+    kitty \
+    liberation-fonts \
+    nautilus \
+    network-manager-applet \
+    pavucontrol \
+    pulseaudio-utils \
+    polkit-gnome \
+    tldr \
+    xdg-user-dirs \
+    xdg-user-dirs-gtk \
+    xdg-desktop-portal-hyprland \
+    wl-clipboard \
+    wlsunset \
+    wlogout \
+    virt-install \
+    libvirt-daemon-config-network \
+    libvirt-daemon-kvm \
+    qemu-kvm \
+    virt-manager \
+    virt-viewer \
+    guestfs-tools \
+    python3-tools \
+    python3-libguestfs \
+    virt-top
 
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
+# Add COPR repositories
+for i in solopasha/hyprland; do
+    MAINTAINER="${i%%/*}"
+    REPOSITORY="${i##*/}"
+    curl --output-dir "/etc/yum.repos.d/" --remote-name \
+    "https://copr.fedorainfracloud.org/coprs/${MAINTAINER}/${REPOSITORY}/repo/fedora-${RELEASE}/${MAINTAINER}-${REPOSITORY}-fedora-${RELEASE}.repo"
+done
 
+rpm-ostree install cliphist hypridle hyprlock hyprshot waypaper hyprland-git hyprpaper hyprshot
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
---
+++ build_iso.sh
@@ -0,0 +1,8 @@
+mkdir ./iso-output
+sudo podman run --rm --privileged --volume ./iso-output:/build-container-installer/build --security-opt label=disable --pull=newer \
+ghcr.io/jasonn3/build-container-installer:latest \
+IMAGE_REPO=ghcr.io/lukedearden \
+VERSION=40 \
+IMAGE_NAME=atomic-fondu \
+IMAGE_TAG=latest \
+VARIANT=base-main # should match the variant your image is based on
